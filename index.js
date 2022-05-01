f1a6015e011e1664e8ac395561d9d3dd6c56fa67
OmarRaihan <omarraihan91@gmail.com>
2022-05-01 23:22:57 +0600

    api for bike added

--- index.js
+++ index.js
@@ -2,22 +2,39 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 7000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+
 // Connect with MongoDB
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bfbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9vvtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("Bike Town DB Connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+// Async Function
+async function run() {
+  try {
+    await client.connect();
+    const bikeCollection = client.db("bikeUser").collection("bike");
+
+    // API for Bike Collection
+    app.get("/bike", async (req, res) => {
+      const query = {};
+      const cursor = bikeCollection.find(query);
+      const bikes = await cursor.toArray();
+      res.send(bikes);
+    });
+  } 
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 // ROOT / Blank API
 app.get("/", (req, res) => {
@@ -26,5 +43,5 @@ app.get("/", (req, res) => {
 
 // Root API Supporter
 app.listen(port, () => {
-  console.log("Listening to port");
+  console.log("Listening to port", port);
 });
